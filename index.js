f2f064c55d5f90b35a0b7be5a704bb93dd1f0f6b
walkerle <118040893+walkerle@users.noreply.github.com>
2022-11-22 15:13:28 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    "name": "employeeName",
+    "streetAddress": "employeeStreetAddress",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = {...obj};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key]
+    return obj;
+}
